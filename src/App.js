373891374a17651392d08499569f33929e6b6304
Samantha Diaz <samidiaz3@gmail.com>
2022-03-09 15:39:08 -0500

    commit tree cda6363d336a62a70d18ffc7a7d87b3714736d6a

--- src/App.js
+++ src/App.js
@@ -11,13 +11,9 @@ class App extends Component {
 
 
   //Your code here:
-
-
-
-
-
-
-
+  componentDidMount() {
+    this.handleAddTimer()
+  }
 
 
   // No need to modify anything in render or the class methods below
